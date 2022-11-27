360a6dffcab383d452e991d8611ca2702eef418b
Th3HMan <36606640+TheHMan1@users.noreply.github.com>
2022-11-27 12:50:29 -0500

    Made counter and multiplier

---
+++ hw5/3bitmult.v
@@ -0,0 +1,8 @@
+module mult3(a, b, out);
+
+input [2:0] a, b;
+output [5:0] c;
+
+assign c = a*b;
+
+endmodule
--- hw5/tff.v
+++ hw5/tff.v
@@ -1,14 +1,14 @@
-module tff(clk, reset, T, Q, notQ);
+module tff(clk, reset, T, Q);
 
 input [0:0] clk, reset, T;
-output reg [0:0] Q, notQ;
-
-assign notQ = !Q;
+output reg [0:0] Q;
 
 always @(posedge clk) begin
     if(reset) Q <= 0;
-    else
+    else if(T)
         Q <= !Q;
+    else
+        Q <= Q;
 end
 
 endmodule
